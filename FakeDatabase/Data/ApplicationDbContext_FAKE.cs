3dc5ca3a5b33ecda62db57446a76ee7e43295859
Richard Chalk <barmybrit@gmail.com>
2024-12-07 16:31:58 +0100

    Added extra fake DbSets<T>

--- FakeDatabase/Data/ApplicationDbContext_FAKE.cs
+++ FakeDatabase/Data/ApplicationDbContext_FAKE.cs
@@ -4,6 +4,10 @@ namespace FakeDatabase.Data;
 
 public class ApplicationDbContext_FAKE
 {
-    // Detta motsvarar ett DbSet<> i EF-Core
+    // Dessa motsvarar DbSet<T> i EF-Core
+    // Varje klass motsvarar en Entitet (tabell) i SQL Databasen!
     public List<Customer> Customers { get; set; }
+    public Order Orders { get; set; }
+    public Product Items { get; set; }
+    public Invoice Invoices { get; set; }
 }
